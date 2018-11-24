0afa2b19414f3e3d46ba2f0eeb7af81836334d62
Kazuma Takata <neilbuzztakata@gmail.com>
2018-11-24 23:08:17 +0900

    fi

---
+++ http/service.go
@@ -0,0 +1,125 @@
+package httpd
+
+import (
+	"encoding/json"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
+
+// Store is the interface Raft-backed key-value stores must implement.
+type Store interface {
+	// Get returns the value for the given key.
+	Increment() error
+
+	// Join joins the node, identitifed by nodeID and reachable at addr, to the cluster.
+	Join(nodeID string, addr string) error
+}
+
+// Service provides HTTP service.
+type Service struct {
+	addr string
+	ln   net.Listener
+
+	store Store
+}
+
+// New returns an uninitialized HTTP service.
+func New(addr string, store Store) *Service {
+	return &Service{
+		addr:  addr,
+		store: store,
+	}
+}
+
+// Start starts the service.
+func (s *Service) Start() error {
+	server := http.Server{
+		Handler: s,
+	}
+
+	ln, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	http.Handle("/", s)
+
+	go func() {
+		err := server.Serve(s.ln)
+		if err != nil {
+			log.Fatalf("HTTP serve: %s", err)
+		}
+	}()
+
+	return nil
+}
+
+// Close closes the service.
+func (s *Service) Close() {
+	s.ln.Close()
+	return
+}
+
+// ServeHTTP allows Service to serve HTTP requests.
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/inc") {
+		s.handleKeyRequest(w, r)
+	} else if r.URL.Path == "/join" {
+		s.handleJoin(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (s *Service) handleJoin(w http.ResponseWriter, r *http.Request) {
+	m := map[string]string{}
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if len(m) != 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	remoteAddr, ok := m["addr"]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	nodeID, ok := m["id"]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := s.store.Join(nodeID, remoteAddr); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Service) handleKeyRequest(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		err := s.store.Increment()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	return
+}
+
+// Addr returns the address on which the Service is listening
+func (s *Service) Addr() net.Addr {
+	return s.ln.Addr()
+}
---
+++ store/store.go
@@ -0,0 +1,195 @@
+package store
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/hashicorp/raft-boltdb"
+)
+
+const (
+	retainSnapshotCount = 2
+	raftTimeout         = 10 * time.Second
+)
+
+type command struct {
+	Op string `json:"op,omitempty"`
+}
+
+// Store is a simple key-value store, where all changes are made via Raft consensus.
+type Store struct {
+	RaftDir  string
+	RaftBind string
+	inmem    bool
+
+	mu sync.Mutex
+	m  int
+
+	raft *raft.Raft // The consensus mechanism
+
+	logger *log.Logger
+}
+
+// New returns a new Store.
+func New(inmem bool) *Store {
+	return &Store{
+		m:      0,
+		inmem:  inmem,
+		logger: log.New(os.Stderr, "[store] ", log.LstdFlags),
+	}
+}
+
+// Open opens the store. If enableSingle is set, and there are no existing peers,
+// then this node becomes the first node, and therefore leader, of the cluster.
+// localID should be the server identifier for this node.
+func (s *Store) Open(enableSingle bool, localID string) error {
+	// Setup Raft configuration.
+	config := raft.DefaultConfig()
+	config.LocalID = raft.ServerID(localID)
+
+	// Setup Raft communication.
+	addr, err := net.ResolveTCPAddr("tcp", s.RaftBind)
+	if err != nil {
+		return err
+	}
+	transport, err := raft.NewTCPTransport(s.RaftBind, addr, 3, 10*time.Second, os.Stderr)
+	if err != nil {
+		return err
+	}
+
+	// Create the snapshot store. This allows the Raft to truncate the log.
+	snapshots, err := raft.NewFileSnapshotStore(s.RaftDir, retainSnapshotCount, os.Stderr)
+	if err != nil {
+		return fmt.Errorf("file snapshot store: %s", err)
+	}
+
+	// Create the log store and stable store.
+	var logStore raft.LogStore
+	var stableStore raft.StableStore
+	if s.inmem {
+		logStore = raft.NewInmemStore()
+		stableStore = raft.NewInmemStore()
+	} else {
+		boltDB, err := raftboltdb.NewBoltStore(filepath.Join(s.RaftDir, "raft.db"))
+		if err != nil {
+			return fmt.Errorf("new bolt store: %s", err)
+		}
+		logStore = boltDB
+		stableStore = boltDB
+	}
+
+	// Instantiate the Raft systems.
+	ra, err := raft.NewRaft(config, (*fsm)(s), logStore, stableStore, snapshots, transport)
+	if err != nil {
+		return fmt.Errorf("new raft: %s", err)
+	}
+	s.raft = ra
+
+	if enableSingle {
+		configuration := raft.Configuration{
+			Servers: []raft.Server{
+				{
+					ID:      config.LocalID,
+					Address: transport.LocalAddr(),
+				},
+			},
+		}
+		ra.BootstrapCluster(configuration)
+	}
+
+	return nil
+}
+
+func (s *Store) Increment(key, value string) error {
+	if s.raft.State() != raft.Leader {
+		return fmt.Errorf("not leader")
+	}
+
+	c := &command{
+		Op: "increment",
+	}
+	b, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	f := s.raft.Apply(b, raftTimeout)
+	return f.Error()
+}
+
+type fsm Store
+
+func (f *fsm) Apply(l *raft.Log) interface{} {
+	var c command
+	if err := json.Unmarshal(l.Data, &c); err != nil {
+		panic(fmt.Sprintf("failed to unmarshal command: %s", err.Error()))
+	}
+
+	switch c.Op {
+	case "increment":
+		return f.applyIncrement()
+	default:
+		panic(fmt.Sprintf("unrecognized command op: %s", c.Op))
+	}
+}
+
+// Snapshot returns a snapshot of the key-value store.
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	return &fsmSnapshot{store: f.m}, nil
+}
+
+// Restore stores the key-value store to a previous state.
+func (f *fsm) Restore(rc io.ReadCloser) error {
+	o := 0
+	if err := json.NewDecoder(rc).Decode(&o); err != nil {
+		return err
+	}
+
+	// Set the state from the snapshot, no lock required according to
+	// Hashicorp docs.
+	f.m = o
+	return nil
+}
+
+func (f *fsm) applyIncrement() interface{} {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.m++
+	return nil
+}
+
+type fsmSnapshot struct {
+	store int
+}
+
+func (f *fsmSnapshot) Persist(sink raft.SnapshotSink) error {
+	err := func() error {
+
+		// Write data to sink.
+		if _, err := sink.Write([]byte{byte(f.store)}); err != nil {
+			return err
+		}
+
+		// Close the sink.
+		return sink.Close()
+	}()
+
+	if err != nil {
+		sink.Cancel()
+	}
+
+	return err
+}
+
+func (f *fsmSnapshot) Release() {}
